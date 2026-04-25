92bfe95123593ebb10ca411f778d5df4c8125131
nehondohudson-rgb <nehondohudson@gmail.com>
2026-04-26 00:00:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,22 @@
+let orders = [];
 
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv").config();
+app.post("/order", (req, res) => {
+  const order = req.body;
 
-const app = express();
+  orders.push({
+    id: Date.now(),
+    items: order.items,
+    total: order.total,
+    status: "pending"
+  });
 
-app.use(cors());
-app.use(express.json());
+  console.log("New Order:", order);
 
-const PORT = process.env.PORT || 3000;
-
-// Test route
-app.get("/", (req, res) => {
-  res.send("Chapati API is running 🚀");
+  res.json({
+    message: "Order received successfully 🚀"
+  });
 });
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
-
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
-app.get("/menu", (req, res) => {
-  res.json([
-    { id: 1, name: "Chapati", price: 20 },
-    { id: 2, name: "Beef Chapati", price: 50 },
-    { id: 3, name: "Egg Chapati", price: 40 },
-    { id: 4, name: "Chicken Chapati", price: 80 }
-  ]);
+app.get("/orders", (req, res) => {
+  res.json(orders);
 });
