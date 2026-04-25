e2864c2aa95b9c4c0046855e25574b15cd6ac8bb
nehondohudson-rgb <nehondohudson@gmail.com>
2026-04-25 22:43:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
 
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Chapati API is running 🚀");
+});
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
