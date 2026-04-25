bcc8b9abd621d33f8e5593fd682d02437f735676
nehondohudson-rgb <nehondohudson@gmail.com>
2026-04-25 22:51:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -23,3 +23,11 @@ mongoose.connect(process.env.MONGO_URI)
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
+app.get("/menu", (req, res) => {
+  res.json([
+    { id: 1, name: "Chapati", price: 20 },
+    { id: 2, name: "Beef Chapati", price: 50 },
+    { id: 3, name: "Egg Chapati", price: 40 },
+    { id: 4, name: "Chicken Chapati", price: 80 }
+  ]);
+});
